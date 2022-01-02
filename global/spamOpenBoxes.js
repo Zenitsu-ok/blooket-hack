eade7cd8e75536f5caf7bbd8a29f4dc8284bf947
Zenitsu-ok <96564169+Zenitsu-ok@users.noreply.github.com>
2022-01-02 18:25:12 +0700

    Update spamOpenBoxes.js

--- global/spamOpenBoxes.js
+++ global/spamOpenBoxes.js
@@ -1,9 +1,16 @@
 const sleep = (ms) => new Promise(r => setTimeout(r, ms));
 
 (async () => {
-    const box = prompt('What box do you want to open? (EXAMPLE: Space)');
+    const box = prompt('What box do you want to open? (EXAMPLE: Spooky)');
     const amount = prompt('How many boxes do you want to open?');
-
+    Aquatic:0
+    Space:0
+    Blizzard:0
+    Spooky:0
+    Bot:0
+    Breakfast:0
+    Medieval:0
+    Wonderland:0
     const response = await fetch('https://api.blooket.com/api/users/verify-token', {
         method: "GET",
         headers: {
